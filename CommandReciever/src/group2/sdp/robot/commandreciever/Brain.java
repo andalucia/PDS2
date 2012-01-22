f0e1252c71e66bbf79851c6a5693d8396f438c67
Chris Howard <chrishoward91@gmail.com>
2012-01-22 19:57:54 +0000

    Added comments to the Brain

--- CommandReciever/src/group2/sdp/robot/commandreciever/Brain.java
+++ CommandReciever/src/group2/sdp/robot/commandreciever/Brain.java
@@ -5,54 +5,74 @@ import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
 
+/**
+ * Alfie's brain
+ * 
+ * This class is Alfie's brain and controls all Alfie's thoughts and actions, methods
+ * from this class should be called by the Client (Alfie's ears)
+ * 
+ * Make sure you run Brain.init() first!
+ */
 public class Brain {
 	
-	private static String fwd1 = "Going forward ";
-	private static String fwd2 = "with speed:"; 
-	private static String stp = "Stopped";
-	private static String kck1 = "Kicking with ";
-	private static String kck2 = "power:";
+	// Alfie's mouth. String constants to be displayed on the LCD, each line is
+	// defined as a different field
+	private static String FWD1 = "Going forward ";
+	private static String FWD2 = "with speed:"; 
+	private static String STP = "Stopped";
+	private static String KCK1 = "Kicking with ";
+	private static String KCK2 = "power:";
 	
-	// constants for the pilot class
+	// Alfie's physical attributes. Constants for the pilot class,
+	// measurements are in centimeters
 	private static final float TRACK_WIDTH = (float) 14.5;
 	private static final float WHEEL_DIAMETER = (float) 8.4;
 	
-	// The motors to be controlled
+	// Alfie's legs and arms. The motors to be controlled
 	private static final NXTRegulatedMotor LEFT_WHEEL = Motor.C;
 	private static final NXTRegulatedMotor RIGHT_WHEEL = Motor.A;
 	private static final NXTRegulatedMotor KICKER = Motor.B;
 	
-	// All motors will be set to this speed initially
+	// The speed to set the kicker motor, determines the power of the kick
 	private static final int KICKER_SPEED = 10000;
 	
-	// Robot state indicators
+	// Alfie's actions. Robot state indicators
 	private static volatile boolean kicking = false;
 	private static DifferentialPilot pilot;
 	
+	/**
+	 * All methods in this class are static so there is no constructor
+	 * This method should be called before running any other method in the class	
+	 */
 	public static void init () {
 		pilot = new DifferentialPilot(WHEEL_DIAMETER, TRACK_WIDTH, LEFT_WHEEL, RIGHT_WHEEL);
 		KICKER.setSpeed(KICKER_SPEED);
 	}
 	
+	/**
+	 * Make Alfie go forwards
+	 * 
+	 * @param speed The speed in cm/s
+	 */
 	public static void goForward(int speed) {
 		LCD.clear();
-		LCD.drawString(fwd1, 0, 0);
-		LCD.drawString(fwd2, 0, 1);
+		LCD.drawString(FWD1, 0, 0);
+		LCD.drawString(FWD2, 0, 1);
 		LCD.drawInt(speed, 1, 2);
 		LCD.drawString("MAX SPEED", 0, 3);
 		LCD.drawInt((int)pilot.getMaxTravelSpeed(), 1, 4);
-		LCD.refresh();
-		
-		
+		LCD.refresh();			
 		
 		pilot.setTravelSpeed(speed);
 		pilot.forward();
 	}
 	
-	
+	/**
+	 * Make Alfie stop moving
+	 */
 	public static void stop() {
 		LCD.clear();
-		LCD.drawString(stp, 0, 0);
+		LCD.drawString(STP, 0, 0);
 		LCD.refresh();
 		
 		pilot.stop();
@@ -62,28 +82,23 @@ public class Brain {
 	 * Kick the ball!
 	 * 
 	 * Starts a new thread and makes the robot kick if it isn't already kicking
+	 * @param power How hard should the motor rotate in degrees/s
 	 */
-	public static void kick(int power) {
-		LCD.clear();
-		LCD.drawString(kck1, 0, 0);
-		LCD.drawString(kck2, 0, 1);
-		LCD.drawInt(power, 1, 2);
-		LCD.refresh();
-		
+	public static void kick(int power) {	
+		// Start a new thread to control the kicker
 		Thread Kick_thread = new Thread() {
-			public void run() {
-				
+	
+			public void run() {			
 				KICKER.rotate(90, true);
 				
 				try {
-					Thread.sleep(1000);
-					
+					Thread.sleep(1000);			
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
-				
+							
 				KICKER.rotate(-90, true);
+				
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {				
@@ -92,9 +107,17 @@ public class Brain {
 				
 				kicking = false;
 			}
+			
 		};
 		
+		// Alfie only has 1 leg, so he can only make 1 kick at a time
 		if (!kicking) {
+			LCD.clear();
+			LCD.drawString(KCK1, 0, 0);
+			LCD.drawString(KCK2, 0, 1);
+			LCD.drawInt(power, 1, 2);
+			LCD.refresh();
+			
 			kicking = true;
 			Kick_thread.start();
 		}	
