4dcf22d38dc439de092a1ced528e4d5a34e081eb
Anda-lucia Flintoaca <s0922504@inishail.inf.ed.ac.uk>
2012-01-25 22:00:37 +0000

    Made the Client class work with CandyPacket as well. Has not been tested yet - if there are problems, roll-back to previous commit, as it was tested and works.

--- CommandReciever/src/group2/sdp/robot/commandreciever/Client.java
+++ CommandReciever/src/group2/sdp/robot/commandreciever/Client.java
@@ -42,13 +42,14 @@ public class Client {
 		
 			boolean reset = false; 
 			while (!exit && !reset) {
-				byte [] b = new byte [CandyPacket.PACKET_SIZE];
-				if (recieveBytes(b)) {
-					int rslt = executeCommand(b);
-					if (rslt == CandyPacket.RESET_CANDY)
+				CandyPacket candy = receiveCandy();
+				if (candy != null) {
+					consumeCandy(candy);
+					if (candy.getBrand() == CandyPacket.RESET_CANDY)
 						reset = true;
-					if (rslt == CandyPacket.SLEEP_CANDY)
+					if (candy.getBrand() == CandyPacket.SLEEP_CANDY)
 						exit = true;
+					giveCandyBack(candy);
 				} else {
 					reset = true;
 				}
@@ -65,76 +66,85 @@ public class Client {
 		}
 	}
 	
+
 	/**
-	 * Receives 32 bytes from the blue-tooth connection. 
-	 * @param b The array in which to store the bytes.
+	 * Receives a packet full of candy over the blue-tooth connection. 
+	 * @return The CandyPacket that was received.
 	 */
-	private static boolean recieveBytes(byte [] b) {
+	private static CandyPacket receiveCandy() {
+		CandyPacket candy = null;
 		try {
-			if (dis.read(b, 0, CandyPacket.PACKET_SIZE) != CandyPacket.PACKET_SIZE)
+			byte [] b = new byte [CandyPacket.PACKET_SIZE];
+			if (dis.read(b, 0, CandyPacket.PACKET_SIZE) != CandyPacket.PACKET_SIZE) {
 				// Connection was closed by the server, without telling Alfie!
-				return false;
+				return null;
+			}
+			candy = new CandyPacket(b);
 		} catch (IOException e) {
 			// Connection was closed by the server, without telling Alfie!
-			return false;
+			return null;
 		}
+		return candy;
+	}
+
+	/**
+	 * Alfie kindly returns the CandyPacket he was given. Unfortunately, he drooled
+	 * on the feedback sweets, so they are modified now.
+	 * @param candy The CandyPacket, modified after being drooled on.
+	 */
+	private static void giveCandyBack(CandyPacket candy) {
 		try {
-			dos.write(b, 0, CandyPacket.PACKET_SIZE);
+			dos.write(candy.getSweets(), 0, CandyPacket.PACKET_SIZE);
 			dos.flush();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return true;
 	}
 	
 	/**
-	 * Converts four bytes of an array of bytes to an integer.
-	 * @param b The array of bytes to convert.
-	 * @param offset The offset at which to get the four bytes
-	 * @return The integer.
+	 * Alfie first reacts on the CandyPacket and then drools on it in order
+	 * to modify its feedback sweets.
+	 * @param candy The candy that was given to Alfie.
 	 */
-	private static int byte4ToInt(byte [] b, int offset) {
-		int result = 0;
-		for (int i = 0; i < 4; ++i) {
-			result <<= 8;
-			result += b[offset + i] < 0 ? (int)b[offset + i] + 256 : (int) b[offset + i];			
-		}
-		return result;
+	private static void consumeCandy(CandyPacket candy) {
+		reactOnCandy(candy);
+		droolOnCandy(candy);
 	}
 	
 	/**
-	 * Deciphers the command that is received and executes it. 
-	 * @param b The commands.
-	 * @return The first byte of the command.
+	 * Makes Alfie react depending on the type of CandyPacket he received.
+	 * @param candy The CandyPacket that was given to Alfie over blue-tooth.
 	 */
-	private static int executeCommand(byte [] b) {
-		int speed = 0, angle;
-		switch (b[0]) {
+	private static void reactOnCandy(CandyPacket candy) {
+		switch (candy.getBrand()) {
 		case CandyPacket.STOP_CANDY:
 			Brain.stop();
 			break;
 		case CandyPacket.GO_FORWARD_CANDY:
-			speed = byte4ToInt(b, 4);
-			Brain.goForward(speed);
+			Brain.goForward(candy.getPretzel(0));
 			break;
 		case CandyPacket.GO_BACKWARDS_CANDY:
-			speed = byte4ToInt(b, 4);     
-			Brain.goBackwards(speed);
+			Brain.goBackwards(candy.getPretzel(0));
 			break;
 		case CandyPacket.SPIN_CANDY:
-			speed = byte4ToInt(b, 4);
-			angle = byte4ToInt(b, 8);
-			Brain.spin(speed, angle);
+			Brain.spin(candy.getPretzel(0), candy.getPretzel(1));
 			break;
-		case CandyPacket.KICK_CANDY:
-			int kick = byte4ToInt(b, 4);     
-			Brain.kick(kick);
+		case CandyPacket.KICK_CANDY:     
+			Brain.kick(candy.getPretzel(0));
 			break;
 		case CandyPacket.RESET_CANDY:
 			break;
 		case CandyPacket.SLEEP_CANDY:
 			break;
 		}
-		return b[0];
+	}
+
+	/**
+	 * Modifies the CandyPacket as a result of drooling over it. The feedback
+	 * sweets and pretzels of the CandyPacket are updated. 
+	 * @param candy The CandyPacket that was given to Alfie.
+	 */
+	private static void droolOnCandy(CandyPacket candy) {
+		// TODO: implement
 	}
 }
