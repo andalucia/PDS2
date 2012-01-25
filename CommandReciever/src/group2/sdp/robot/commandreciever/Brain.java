c3cfcee2280a86fae34e4f71f1604d6f461a565f
Stanislav Manilov <stanislav.manilov@gmail.com>
2012-01-25 16:21:21 +0000

    Moved pilot actions before LED output, which should, in theory, improve response time.

--- CommandReciever/src/group2/sdp/robot/commandreciever/Brain.java
+++ CommandReciever/src/group2/sdp/robot/commandreciever/Brain.java
@@ -22,6 +22,7 @@ public class Brain {
 	private static String STP = "Stopped.";
 	private static String KCK1 = "Kicking with ";
 	private static String KCK2 = "power:";
+	private static String SPN = "Spinning around...";
 	
 	// Alfie's physical attributes. Constants for the pilot class,
 	// measurements are in centimetres.
@@ -58,52 +59,44 @@ public class Brain {
 	 */
 	public static void goForward(int speed) {
 		assert(initialized);
+		
+		pilot.setTravelSpeed(speed);
+		pilot.forward();
+		
 		LCD.clear();
 		LCD.drawString(FWD1, 0, 0);
 		LCD.drawString(FWD2, 0, 1);
 		LCD.drawInt(speed, 1, 2);
 		LCD.drawString("MAX SPEED", 0, 3);
 		LCD.drawInt((int)pilot.getMaxTravelSpeed(), 1, 4);
-		LCD.refresh();			
-		
-		pilot.setTravelSpeed(speed);
-		pilot.forward();
+		LCD.refresh();
 	}
 	
 	/**
-	 * This method is for reference
+	 * This method is for reference.
 	 */
 	public static void spin() {
 		assert(initialized);
-		LCD.clear();
-		LCD.drawString(FWD1, 0, 0);
-		LCD.drawString(FWD2, 0, 1);
-		//LCD.drawInt(speed, 1, 2);
-		LCD.drawString("MAX SPEED", 0, 3);
-		LCD.drawInt((int)pilot.getMaxTravelSpeed(), 1, 4);
-		LCD.refresh();			
 		
 		pilot.setTravelSpeed(20);
 		pilot.arc(100, 90);
 		
-		/*LEFT_WHEEL.setSpeed(90);
-		RIGHT_WHEEL.setSpeed(45);
-		LEFT_WHEEL.forward();
-		RIGHT_WHEEL.forward();*/
+		LCD.clear();
+		LCD.drawString(SPN, 0, 0);
+		LCD.refresh();
 	}
 	
-	
-	
 	/**
-	 * Make Alfie stop moving
+	 * Make Alfie stop moving.
 	 */
 	public static void stop() {
 		assert(initialized);
+		
+		pilot.stop();
+		
 		LCD.clear();
 		LCD.drawString(STP, 0, 0);
 		LCD.refresh();
-		
-		pilot.stop();
 	}
 	
 	/**
@@ -141,14 +134,14 @@ public class Brain {
 		
 		// Alfie only has 1 leg, so he can only make 1 kick at a time
 		if (!kicking) {
+			kicking = true;
+			Kick_thread.start();
+			
 			LCD.clear();
 			LCD.drawString(KCK1, 0, 0);
 			LCD.drawString(KCK2, 0, 1);
 			LCD.drawInt(power, 1, 2);
 			LCD.refresh();
-			
-			kicking = true;
-			Kick_thread.start();
 		}	
 	}
 }
