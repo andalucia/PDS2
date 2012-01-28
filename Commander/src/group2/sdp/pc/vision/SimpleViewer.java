2f5c49151ac077f85830042c1383a795f19ab58f
Paul Thomson <paul041091@hotmail.co.uk>
2012-01-28 15:56:57 +0000

    Vision can now track ball and marks a cross on the position

--- Commander/src/group2/sdp/pc/vision/SimpleViewer.java
+++ Commander/src/group2/sdp/pc/vision/SimpleViewer.java
@@ -1,8 +1,10 @@
 package group2.sdp.pc.vision;
 
+import java.awt.Point;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,7 +19,8 @@ import au.edu.jcu.v4l4j.exceptions.StateException;
 import au.edu.jcu.v4l4j.exceptions.V4L4JException;
 
 public class SimpleViewer extends WindowAdapter implements CaptureCallback{
-	private static int      width = 640, height = 480, std = V4L4JConstants.STANDARD_WEBCAM, channel = 0;
+	private static int      width = 640, height = 480, std = V4L4JConstants.STANDARD_WEBCAM, channel = 0,
+	width_margin = 20, height_margin = 60;
 	private static String   device = "/dev/video0";
 
 	private VideoDevice     videoDevice;
@@ -25,6 +28,7 @@ public class SimpleViewer extends WindowAdapter implements CaptureCallback{
 
 	private JLabel          label;
 	private JFrame          frame;
+	private int[] red = new int[] { 255, 0, 0 };
 
 
 
@@ -143,19 +147,59 @@ public class SimpleViewer extends WindowAdapter implements CaptureCallback{
 	 }
 
 	 public BufferedImage normalizeFrame(BufferedImage thisFrame) {
-		 // does not normalize at the moment (thresholds for red)
-//		 for (int x=0; x < thisFrame.getWidth(); x++)
-//		 {          
-//			 for (int y = 0; y < thisFrame.getHeight(); y++)
-//			 {   
-//				 //System.out.println((thisFrame.getRGB(x,y) >> 16) & 0xFF);
-//				 if (( (thisFrame.getRGB(x,y) >> 16) & 0xFF) > 210) {
-//					 thisFrame.setRGB(x, y, 0xFFFFFF);
-//				 } else {
-//					 thisFrame.setRGB(x, y, 0x000000); }
-//			 }
-//		 }
+		 //does not normalize at the moment (thresholds for red)
+		 WritableRaster cross = thisFrame.getRaster();
+		 Point ball_pos = new Point(0,0);
+		 int redness = -1;
+		 for (int x=width_margin; x < thisFrame.getWidth() - width_margin; x++)
+		 {          
+			 for (int y = height_margin; y < thisFrame.getHeight() - height_margin; y++)
+			 {   
+				 //System.out.println((thisFrame.getRGB(x,y) >> 16) & 0xFF);
+				 if (( (thisFrame.getRGB(x,y) >> 16) & 0xFF) > redness) {
+					 //System.out.println(((thisFrame.getRGB(x,y) >> 16) & 0xFF) + " > " + redness);
+					 ball_pos.x = x;
+					 ball_pos.y = y;
+					 redness = (thisFrame.getRGB(x,y) >> 16) & 0xFF;
+				 }
+			 }
+		 }
+		 drawCross(cross, ball_pos, red);
 		 return thisFrame;
 
 	 }
+	 
+	 /**
+		 * A safe way to draw a pixel
+		 * 
+		 * @param raster
+		 *            draw on writable raster
+		 * @param p1
+		 *            point coordinates
+		 * @param colour
+		 *            colour
+		 */
+		private void drawPixel(WritableRaster raster, Point p1, int[] colour) {
+			if (p1.x >= 0 && p1.x < width && p1.y >= 0 && p1.y < height)
+				raster.setPixel(p1.x, p1.y, colour);
+		}
+		
+		/**
+		 * Draws 2 perpendicular lines intersecting at point p1
+		 * 
+		 * @param raster
+		 *            draw on writable raster
+		 * @param p1
+		 *            point coordinates
+		 * @param colour
+		 *            colour
+		 */
+		private void drawCross(WritableRaster raster, Point p1, int[] colour) {
+			for (int i = 0; i < width; i++) {
+				drawPixel(raster, new Point(i, p1.y), colour);
+			}
+			for (int i = 0; i < height; i++) {
+				drawPixel(raster, new Point(p1.x, i), colour);
+			}
+		}
 }
