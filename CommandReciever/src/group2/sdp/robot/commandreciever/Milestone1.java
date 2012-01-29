0c5ae7a490829e6a13b68e5835cfda6a017ae96d
Stanislav Manilov <stanislav.manilov@gmail.com>
2012-01-29 18:19:07 +0000

    Added the class that should contain the NXT program for Milestone1

---
+++ CommandReciever/src/group2/sdp/robot/commandreciever/Milestone1.java
@@ -0,0 +1,18 @@
+package group2.sdp.robot.commandreciever;
+
+/**
+ * This class presents a menu for selecting which of the Milestone1 tasks should 
+ * be executed (Cross Pitch or Kick).
+ */
+public class Milestone1 {
+	
+	public static void main (String [] args) {
+		// Initiate BRAIN!
+        Brain.init();
+        
+        boolean exit = false;
+        while (!exit) {
+        	exit = true;
+        }
+	}	
+}
