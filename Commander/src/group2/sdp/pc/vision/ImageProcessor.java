44db87f40f2088bd00e08017759896de72c914c8
Paul Thomson <paul041091@hotmail.co.uk>
2012-02-02 18:51:31 +0000

    Abstracted image grabber and added image processing without GUI

---
+++ Commander/src/group2/sdp/pc/vision/ImageProcessor.java
@@ -0,0 +1,13 @@
+package group2.sdp.pc.vision;
+
+import java.awt.image.BufferedImage;
+
+public class ImageProcessor {
+	
+	public static void process(BufferedImage image) {
+		//add methods to detect robots etc
+	}
+	
+	
+
+}
