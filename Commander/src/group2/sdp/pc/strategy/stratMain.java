5d0b7a0f12d02ee21ab371ddf2540acd70eb13bb
Shaun Mc closkey <s0930584@bugatti.inf.ed.ac.uk>
2012-02-02 15:02:40 +0000

    changed call to Viewer to simpleViewer

--- Commander/src/group2/sdp/pc/strategy/stratMain.java
+++ Commander/src/group2/sdp/pc/strategy/stratMain.java
@@ -17,7 +17,7 @@ public class stratMain extends Thread {
 	// Server for controlling Alfi
 	private Server alfiServer;
 	
-	private SimpleViewer viewer;
+	//private SimpleViewer viewer;
 	public Point ball;
 	public robot Alfi;
 	public robot opponent;
@@ -163,7 +163,7 @@ public class stratMain extends Thread {
 	
 	
 	public boolean closeToTarget(){
-		int distance = (int) viewer.calcDistanceBetweenPoints(Alfi.getCoors(), target);
+		int distance = (int) SimpleViewer.calcDistanceBetweenPoints(Alfi.getCoors(), target);
 		if(distance> 10){
 			return true;
 		}else{
