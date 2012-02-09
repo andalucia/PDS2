096eda660ec66b56e0ab3edc2ec68553f19303b2
Shaun Mc closkey <s0930584@torosay.inf.ed.ac.uk>
2012-02-09 15:27:00 +0000

    the dribbleCommand class actually does stuff

--- Commander/src/group2/sdp/pc/planner/commands/DribbleCommand.java
+++ Commander/src/group2/sdp/pc/planner/commands/DribbleCommand.java
@@ -4,13 +4,31 @@ import java.awt.geom.Point2D;
 
 public class DribbleCommand implements ComplexCommand{
 
+	Point2D ball;
+	Point2D alfie;
+	double facing;
+	
 	public DribbleCommand(Point2D ball, Point2D alfie, double facing) {
-		// TODO Auto-generated constructor stub
 		
-		
+		this.ball = ball;
+		this.alfie = alfie;
+		this.facing = facing;
 		
 	}
 
+	
+	public Point2D getBall(){
+		return ball;
+	}
+	
+	public Point2D getAlfie(){
+		return alfie;
+	}
+	
+	public double getFacing(){
+		return facing;
+	}
+	
 	@Override
 	public Type getType() {
 		// TODO Auto-generated method stub
