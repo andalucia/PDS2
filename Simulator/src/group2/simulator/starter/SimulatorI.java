25dffd24a5dc1b99f2e097431f3d2738710c3b3f
Anda-lucia Flintoaca <s0922504@rothesay.inf.ed.ac.uk>
2012-02-12 17:43:50 +0000

    fogot to add something in the simualtor

--- Simulator/src/group2/simulator/starter/SimulatorI.java
+++ Simulator/src/group2/simulator/starter/SimulatorI.java
@@ -18,6 +18,8 @@ import java.awt.Checkbox;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.WindowAdapter;
