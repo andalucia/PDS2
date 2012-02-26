746cbee9558d1138f3d120133edd2f4f85a7909a
Shaun Mc closkey <s0930584@americano.inf.ed.ac.uk>
2012-02-26 15:36:49 +0000

    overlord class changed

--- Commander/src/group2/sdp/pc/planner/Overlord.java
+++ Commander/src/group2/sdp/pc/planner/Overlord.java
@@ -1,6 +1,10 @@
 package group2.sdp.pc.planner;
 
+import java.awt.geom.Point2D;
+
+import group2.sdp.pc.breadbin.DynamicBallInfo;
 import group2.sdp.pc.breadbin.DynamicPitchInfo;
+import group2.sdp.pc.breadbin.DynamicRobotInfo;
 import group2.sdp.pc.planner.strategy.Strategy;
 import group2.sdp.pc.vision.skeleton.DynamicInfoConsumer;
 
@@ -70,7 +74,62 @@ public class Overlord implements DynamicInfoConsumer {
 	 * @return The strategy that should be currently employed.
 	 */
 	protected Strategy computeStrategy(DynamicPitchInfo dpi) {
-		// TODO: implement
+		DynamicRobotInfo alfieInfo = dpi.getAlfieInfo();
+		DynamicRobotInfo opponentInfo = dpi.getOpponentInfo();
+		DynamicBallInfo ballInfo = dpi.getBallInfo();
+		
+		Point2D ball = ballInfo.getPosition();  
+		
+		if(hasBall(opponentInfo, ball) && correctSide(opponentInfo, ball)){
 		return Strategy.DEFENSIVE;
+		}else{
+			return Strategy.OFFENSIVE;
+		}
+	}
+	
+	
+	/**
+	 * checks to see it the robot is with a certain distance of the ball,
+	 * if it is then this robot has the ball, so return true
+	 * @param robot is the Dynamic pitch info of robot were checking
+	 * @param ball postion of the ball
+	 * @return boolean
+	 */
+	public boolean hasBall(DynamicRobotInfo robot, Point2D ball){
+		Point2D robotPos = robot.getPosition(); 
+		
+		if(robotPos.distance(ball)<=40){
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	
+	/**
+	 * checks to see if the robot is on the right side of the ball, does this by
+	 * comparing the distance along x axis of the robot to its goal line and distance of ball to goal line.
+	 * if the robot is closer its on the right/correct side so return true 
+	 * @param robotInfo
+	 * @param ballPos
+	 * @return
+	 */
+	public boolean correctSide(DynamicRobotInfo robotInfo, Point2D ballPos){
+		Point2D TopGoal = robotInfo.getTopGoalPost();
+		double goalLine = TopGoal.getX();
+		
+		Point2D robotPos = robotInfo.getPosition();
+		double robot = robotPos.getX();
+		double ball = ballPos.getX();
+		
+		double robotDis = Math.abs(goalLine - robot);
+		double ballDis = Math.abs(goalLine - ball);
+		
+		if(robotDis < ballDis ){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
