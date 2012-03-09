f896570ddf92b15d5a2b87df8e0dd09766aa5eac
Stanislav Manilov <stanislav.manilov@gmail.com>
2012-03-09 18:38:35 +0000

    Changed the structure of the getFPS funtcion. Added sample code.

--- Commander/src/group2/sdp/pc/controlstation/Conversation.java
+++ Commander/src/group2/sdp/pc/controlstation/Conversation.java
@@ -11,15 +11,49 @@ public class Conversation {
 	
 	public static void main (String [] args) throws Exception {
 		Mouth s = new Mouth();
-			
+//		s.sendExit();
 		
+//		s.sendGoForward(35, 10);
+//		Thread.sleep(4000);
+//		s.sendSpinLeft(0, 90);
+//		Thread.sleep(4000);
+//		s.sendGoForward(35,10);
+//		Thread.sleep(4000);
+//		s.sendSpinRight(0,90);
+//		Thread.sleep(4000);
+//		s.sendGoForward(35, 30);
+//		s.sendSpinRight(0, 90);
+//		Thread.sleep(4000);
+//		s.sendGoForward(35, 30);
+//		Thread.sleep(4000);
+//		s.sendSpinRight(0,90);
+//		
+//		Paul's Test ^^^
+		
+		long prev = 0, now = 0;
+		for (int i = 0; i < 2000; ++i) {
+			s.sendGoForward(15, 0);
+			Thread.sleep(40); // 5 FPS
+			now = System.currentTimeMillis();
+			System.out.println(getFPS(prev, now));
+			prev = now;
+		}
+		
+		s.sendStop();
+		Thread.sleep(1000);
+		s.sendReset();
+		Thread.sleep(1000);
+		s.cleanup();
 	}
 	
 	/**
-	 * @param prevSysTime - previous state System time in Miliseconds
-	 * @return FPS - frames per second.
+	 * Converts time difference to frames per second.
+	 * @param before System time, in milliseconds, of the beginning of the
+	 * interval.
+	 * @param now System time, in milliseconds, of the end of the interval.
+	 * @return FPS rate, based on the given period.
 	 */
-	public static double getFPS(long prevSysTime){
-		return (1000.0 / (System.currentTimeMillis() - prevSysTime));
+	public static double getFPS(long before, long now){
+		return (1000.0 / (now - before));
 	}
 }
