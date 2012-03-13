a1b2080d847cefdf9b090c82c4d8ec51d1bd170a
Paul Thomson <paul041091@hotmail.co.uk>
2012-03-13 14:32:49 +0000

    OOPS forgot to add files

---
+++ Commander/src/group2/sdp/pc/planner/OperationConsumer.java
@@ -0,0 +1,18 @@
+package group2.sdp.pc.planner;
+
+import group2.sdp.pc.planner.operation.Operation;
+
+/**
+ * A class implementing this interface will consume an Operation
+ * @author Alfie
+ *
+ */
+public interface OperationConsumer {
+
+	/**
+	 * Consumes the operation
+	 * @param operation The operation to be consumed
+	 */
+	public void consumeOperation(Operation operation);
+	
+}
---
+++ Commander/src/group2/sdp/pc/planner/StrategyConsumer.java
@@ -0,0 +1,18 @@
+package group2.sdp.pc.planner;
+
+import group2.sdp.pc.planner.strategy.Strategy;
+
+/**
+ * Classes implementing this interface can consume a Strategy
+ * @author: Alfie
+ *
+ */
+public interface StrategyConsumer {
+	
+	/**
+	 * Sets the strategy
+	 * @param strategy The strategy to set to
+	 */
+	public void setStrategy(Strategy strategy);
+	
+}
