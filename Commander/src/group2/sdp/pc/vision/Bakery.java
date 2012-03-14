2e92af404fce58520be865ecbd7cb94fcd8f3335
Robbie Bailey <peanut_merchant@hotmail.co.uk>
2012-03-14 17:02:29 +0000

    refactored bakery added necessary functions, added correction of bision values to design.txt

--- Commander/src/group2/sdp/pc/vision/Bakery.java
+++ Commander/src/group2/sdp/pc/vision/Bakery.java
@@ -36,7 +36,12 @@ public class Bakery extends BakerySkeleton {
 	/**
 	 * moving is minimum distance an object has moved (between average points) to be considered moving
 	 */
-	private static final double MOVING=0.1;
+	private static final double MOVING_THRESHOLD=0.1;
+	
+	/**
+	 * NOTROTATING is minimum angle a robot has turned to be considered turning 
+	 */
+	private static final double ROTATING_THRESHOLD = 0.1;
 
 	public Bakery(DynamicInfoConsumer consumer) {
 		super(consumer);
@@ -63,7 +68,7 @@ public class Bakery extends BakerySkeleton {
 		long oldTime = findAverageTime(ballHistoryInfos, ballHistoryInfos.size()-4 , ballHistoryInfos.size()-6);
 
 		// test if ball is moving
-		if (oldAvg.distance(recentAvg) < MOVING) {
+		if (oldAvg.distance(recentAvg) < MOVING_THRESHOLD) {
 			return 0;
 		}
 
@@ -93,7 +98,7 @@ public class Bakery extends BakerySkeleton {
 		Point2D recentAvg = findAverageBallPoint(ballHistoryInfos, numPoints-1 , numPoints-3);
 		Point2D oldAvg = findAverageBallPoint(ballHistoryInfos, numPoints-4 , numPoints-6);		
 		// test if ball is moving
-		if (oldAvg.distance(recentAvg) < MOVING) {
+		if (oldAvg.distance(recentAvg) < MOVING_THRESHOLD) {
 			return 0;
 		}
 		// now calculate angle
@@ -133,7 +138,7 @@ public class Bakery extends BakerySkeleton {
 		long recentTime = findAverageTimeR(historyInfos, numPoints-1 , numPoints-3);
 		long oldTime = findAverageTimeR(historyInfos, numPoints-4 , numPoints-6);
 		// test if robot is moving
-		if (oldAvg.distance(recentAvg) < MOVING) {
+		if (oldAvg.distance(recentAvg) < MOVING_THRESHOLD) {
 			return 0;
 		}
 		double timeDif = (recentTime - oldTime);
@@ -144,23 +149,27 @@ public class Bakery extends BakerySkeleton {
 
 	protected double correctRobotFacingDirection(
 			LinkedList<StaticRobotInfo> historyInfos) {
-		// TODO: add to design spec. and audit code
-		// TODO check for negative angles (i.e. when robot is missing)
 		// if we are correcting more than half the frames in the last 10 frames
 		// then
 		// set method to stop correcting for next 10 frames to avoid loop. use
 		// vision values instead
-		// use seperate counters for different robots.
+		// use seperate counters for different robots (i.e Alfie and not Alfie).
+		
+		double currentFacing = historyInfos.getLast().getFacingDirection();
+		//check for negative values (i.e robot is missing)
+		if (currentFacing < 0) {
+			return currentFacing;
+		}
 		if (historyInfos.getLast().isAlfie()) {
 			if (stopAflie > 0) {
 				stopAflie--;
-				return historyInfos.getLast().getFacingDirection();
+				return currentFacing;
 			}
 			if (correctionsAlfie >= 5) {
 				correctionsAlfie = 0;
 				counterAlfie = 0;
 				stopAflie = 10;
-				return historyInfos.getLast().getFacingDirection();
+				return currentFacing;
 			}
 			if (counterAlfie == 10) {
 				counterAlfie = 0;
@@ -174,13 +183,13 @@ public class Bakery extends BakerySkeleton {
 		} else {
 			if (stopOp > 0) {
 				stopOp--;
-				return historyInfos.getLast().getFacingDirection();
+				return currentFacing;
 			}
 			if (correctionsOp >= 5) {
 				correctionsOp = 0;
 				counterOp = 0;
 				stopOp = 10;
-				return historyInfos.getLast().getFacingDirection();
+				return currentFacing;
 			}
 			if (counterOp == 10) {
 				counterOp = 0;
@@ -194,18 +203,18 @@ public class Bakery extends BakerySkeleton {
 		}
 
 		if (historyInfos.size() < 6) {
-			return historyInfos.getLast().getFacingDirection();
+			return currentFacing;
 		}
 		int numSim = 0;
 		// alternative used to pass previous angle if current is extreme
 		double alternative = 0.0;
-		double angle = historyInfos.getLast().getFacingDirection();
+		double angle = currentFacing;
 		// loop through previous points and count how many are similar
 		for (int i = historyInfos.size() - 2; i >= historyInfos.size() - 6
 				&& i >= 1; i--) {
 			double angleOld = historyInfos.get(i).getFacingDirection();
 			// current threshold=90
-			if (isSimilarAngle(angleOld, angle, 90)) {
+			if (isSimilarAngle(angleOld , angle, 90)) {
 				numSim++;
 			} else {
 				alternative = angleOld;
@@ -245,7 +254,7 @@ public class Bakery extends BakerySkeleton {
 		Point2D oldAvg = findAverageRobotPoint(historyInfos, numPoints-4 , numPoints-6);
 		
 		// test if robot is moving
-		if (oldAvg.distance(recentAvg) < MOVING) {
+		if (oldAvg.distance(recentAvg) < MOVING_THRESHOLD) {
 			return historyInfos.getLast().getFacingDirection();
 		}
 		// now calculate angle
@@ -264,15 +273,75 @@ public class Bakery extends BakerySkeleton {
 	@Override
 	protected double computeRobotRotatingSpeed(
 			LinkedList<StaticRobotInfo> historyInfos) {
-		// TODO: Add code that does that
-		return 0;
+		
+		StaticRobotInfo recentRobot = historyInfos.get(historyInfos.size()-1);
+		StaticRobotInfo oldRobot = historyInfos.get(historyInfos.size()-3);
+		double time2 = recentRobot.getTimeStamp();
+		double time1 = oldRobot.getTimeStamp();
+		double angle2 = recentRobot.getFacingDirection();
+		double angle1 = oldRobot.getFacingDirection();
+		double bigAngle;
+		double smallAngle;
+		double angleDifference;
+		
+		// get time difference (seconds)
+		double timeDifference = time2 - time1;
+		timeDifference = timeDifference/1000.0;
+		if (angle2==angle1){
+			return 0;
+		}
+		if (angle1 >= angle2) {
+			bigAngle = angle1;
+			smallAngle = angle2;
+		} else {
+			bigAngle = angle2;
+			smallAngle = angle1;
+		}
+		
+		angleDifference=bigAngle-smallAngle;
+		if((angleDifference) > 180){
+			angleDifference = 360 - angleDifference;
+		}
+		if(verbose){
+			System.out.println("Difference in angle between frames = " +angleDifference);
+		}
+		double rotSpeed = angleDifference / timeDifference;
+		return rotSpeed;
+		
 	}
 
 	@Override
 	protected boolean isRobotRotatingCCW(
 			LinkedList<StaticRobotInfo> historyInfos) {
-		// TODO: Add code that does that
-		return false;
+		
+		StaticRobotInfo recentRobot = historyInfos.get(historyInfos.size()-1);
+		StaticRobotInfo oldRobot = historyInfos.get(historyInfos.size()-3);
+		double angle2 = recentRobot.getFacingDirection();
+		double angle1 = oldRobot.getFacingDirection();
+		//not rotating case
+		if(isSimilarAngle(angle2, angle1 , ROTATING_THRESHOLD)){
+			return false;
+		}
+		if (angle2 > angle1) {
+			if (angle2 <= (angle1 +180)){
+				return true;
+			}else {
+				return false;
+			}
+		}
+		//Else solves 0-360 jump problem
+		//Works out difference between angles in the counter clock wise direction 
+		//and checks difference <180
+		else{
+			double ccwDifference = (360-angle1) + angle2;
+			if (ccwDifference <= 180){
+				return true;
+			}else{
+				return false;
+			}
+					
+		}		
+		
 	}
 
 
